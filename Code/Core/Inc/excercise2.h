d9c69711fac16e29060834341481fcb931c654be
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-09-09 16:56:00 +0700

    update for ex2

--- Code/Core/Inc/excercise2.h
+++ Code/Core/Inc/excercise2.h
@@ -9,5 +9,12 @@
 #define INC_EXCERCISE2_H_
 #include "main.h"
 
+void LED_RED_ON();
+void LED_RED_OFF();
+void LED_YELLOW_ON();
+void LED_YELLOW_OFF();
+void LED_GREEN_ON();
+void LED_GREEN_OFF();
+void run_excercise2();
 
 #endif /* INC_EXCERCISE2_H_ */
--- Code/Core/Src/excercise2.c
+++ Code/Core/Src/excercise2.c
@@ -6,3 +6,48 @@
  */
 #include "excercise2.h"
 
+void LED_RED_ON() {
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+}
+
+void LED_YELLOW_ON() {
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+}
+
+void LED_GREEN_ON() {
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+}
+
+void LED_RED_OFF() {
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+}
+
+void LED_YELLOW_OFF() {
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+}
+
+void LED_GREEN_OFF() {
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+}
+
+void run_excercise2() {
+	LED_RED_OFF();
+	LED_YELLOW_OFF();
+	LED_GREEN_OFF();
+
+	LED_RED_ON();
+	LED_YELLOW_OFF();
+	LED_GREEN_OFF();
+	HAL_Delay(5000);
+
+	LED_RED_OFF();
+	LED_YELLOW_ON();
+	LED_GREEN_OFF();
+	HAL_Delay(2000);
+
+	LED_RED_OFF();
+	LED_YELLOW_OFF();
+	LED_GREEN_ON();
+	HAL_Delay(3000);
+}
+
