0265d2d2191521aaf0b0661fe6d94fd84758c0fa
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-09-09 22:58:59 +0700

    init for ex3

---
+++ Code/Core/Inc/excercise3.h
@@ -0,0 +1,13 @@
+/*
+ * excercise3.h
+ *
+ *  Created on: 9 thg 9, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_EXCERCISE3_H_
+#define INC_EXCERCISE3_H_
+#include "main.h"
+
+
+#endif /* INC_EXCERCISE3_H_ */
---
+++ Code/Core/Src/excercise3.c
@@ -0,0 +1,8 @@
+/*
+ * excercise3.c
+ *
+ *  Created on: 9 thg 9, 2024
+ *      Author: ACER
+ */
+#include "excercise3.h"
+
