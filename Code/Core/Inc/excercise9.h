44f8c53aade5a5c5e82b2fe14e39b5ea9b864c31
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-09-10 21:57:28 +0700

    update for ex9

---
+++ Code/Core/Inc/excercise9.h
@@ -0,0 +1,14 @@
+/*
+ * excercise9.h
+ *
+ *  Created on: Sep 10, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_EXCERCISE9_H_
+#define INC_EXCERCISE9_H_
+#include "main.h"
+
+void clearNumberOnClock(int num);
+
+#endif /* INC_EXCERCISE9_H_ */
