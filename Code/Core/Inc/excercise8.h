bf74953f6c3a9a50e624e322c0d41d6ca6033d6a
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-09-10 21:46:34 +0700

    update ex8

---
+++ Code/Core/Inc/excercise8.h
@@ -0,0 +1,14 @@
+/*
+ * excercise8.h
+ *
+ *  Created on: Sep 10, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_EXCERCISE8_H_
+#define INC_EXCERCISE8_H_
+#include "main.h"
+
+void setNumberOnClock(int num);
+
+#endif /* INC_EXCERCISE8_H_ */
