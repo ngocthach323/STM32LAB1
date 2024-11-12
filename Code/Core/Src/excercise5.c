5219b3d6aa257ce84a1b1035ca41c0168a9669f6
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-11-12 12:14:39 +0700

    complete for ex5

--- Code/Core/Src/excercise5.c
+++ Code/Core/Src/excercise5.c
@@ -7,16 +7,6 @@
 #include "excercise5.h"
 
 void display7SEG_1(int num) {
-	if (num == 0) {
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
-	}
-
 	if (num == 1) {
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
@@ -55,6 +45,16 @@ void display7SEG_1(int num) {
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+	}
+
+	if (num == 5) {
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
 	}
 }
 
@@ -163,11 +163,12 @@ void green_light_2() {
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
 }
+
 void display_1(int *state_1, int *count_1) {
 	switch (*state_1) {
 		case 0:
-		  	display7SEG_1(4 - *count_1);
-		  	red_light_1();
+		  	display7SEG_1(5 - *count_1);
+		  	red_light_1(); //turn on red led
 		  	(*count_1)++;
 		  	if (*count_1 >= 5) {
 		  		*count_1 = 0;
@@ -175,24 +176,22 @@ void display_1(int *state_1, int *count_1) {
 		  	}
 		  	break;
 		case 1:
-		  	display7SEG_1(1 - *count_1);
-		  	yellow_light_1();
-		  	(*count_1)++;
-		  	if (*count_1 >= 2) {
-		  		*count_1 = 0;
-		  		*state_1 = 2;
-		  	}
-		  	break;
-		case 2:
-		  	display7SEG_1(2 - *count_1);
-		  	green_light_1();
+		  	display7SEG_1(3 - *count_1);
+		  	green_light_1(); //turn on green led
 		  	(*count_1)++;
 		  	if (*count_1 >= 3) {
 		  		*count_1 = 0;
 		  		*state_1 = 0;
 		  	}
 		  	break;
-		default:
+		case 2:
+		  	display7SEG_1(2 - *count_1);
+		  	yellow_light_1(); //turn on yellow led
+		  	(*count_1)++;
+		  	if (*count_1 >= 2) {
+		  		*count_1 = 0;
+		  		*state_1 = 2;
+		  	}
 		  	break;
 	}
 }
@@ -200,7 +199,7 @@ void display_1(int *state_1, int *count_1) {
 void display_2(int *state_2, int *count_2) {
 	switch (*state_2) {
 		case 0:
-		  	display7SEG_2(2 - *count_2);
+		  	display7SEG_2(3 - *count_2);
 		  	green_light_2();
 		  	(*count_2)++;
 		  	if (*count_2 >= 3) {
@@ -209,7 +208,7 @@ void display_2(int *state_2, int *count_2) {
 		  	}
 		  	break;
 		case 1:
-		  	display7SEG_2(1 - *count_2);
+		  	display7SEG_2(2 - *count_2);
 		  	yellow_light_2();
 		  	(*count_2)++;
 		  	if (*count_2 >= 2) {
@@ -218,15 +217,13 @@ void display_2(int *state_2, int *count_2) {
 		  	}
 		  	break;
 		case 2:
-		  	display7SEG_2(4 - *count_2);
+		  	display7SEG_2(5 - *count_2);
 		  	red_light_2();
 		  	(*count_2)++;
 		  	if (*count_2 >= 5) {
 		  		*count_2 = 0;
 		  		*state_2 = 0;
 		  	}
-		  	break;
-		default:
 		  	break;
 	}
 }
