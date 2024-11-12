ecbfd580a6ff39845993db8568dfb56aaccb8b60
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-11-12 11:50:55 +0700

    complete for ex4

--- Code/Core/Src/main.c
+++ Code/Core/Src/main.c
@@ -64,7 +64,7 @@ static void MX_GPIO_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-int counter = 0;
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -91,6 +91,7 @@ int counter = 0;
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int counter = 0;
   while (1)
   {
     /* USER CODE END WHILE */
